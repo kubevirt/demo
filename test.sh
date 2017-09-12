9ec6dd734c47554d461c4aa6a6c2f1a300bfd950
Fabian Deutsch <fabiand@fedoraproject.org>
2017-09-12 16:54:12 +0200

    Improve test
    Check for API presence
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -10,22 +10,6 @@ fail() { red "ERR\nFAIL" ; exit 2 ; }
 check() { echo -n "$1 ... "; eval "$2" && ok || fail ; }
 
 
-check_readme_version() {
-  bash -n run-demo.sh || die "demo has syntax errors"
-  eval "$(grep "export GIT_TAG" run-demo.sh)"
-  grep -q $GIT_TAG README.md
-}
-
-check_websocket() {
-  local IP=$(minikube ip) ;
-  curl -s -i -N -H "Connection: Upgrade" \
-  http://$IP:8184/apis/kubevirt.io/v1alpha1/namespaces/default/vms/testvm/console | grep -q "Sec-Websocket-Version: 13" 
-}
-
-
-check "README contains correct version" "check_readme_version"
 check "VM is running" "( kubectl get vm testvm -o jsonpath='{.status.phase}' ) | grep -q Running"
-check "VM serial console works" "check_websocket"
-
-
+check "VM API is present" "kubectl api-versions | grep -q kubevirt.io"
 green PASS
