3e89737aee35b11485ff567c4fd3738a22cac6ad
Fabian Deutsch <fabiand@fedoraproject.org>
2018-09-06 13:24:26 +0200

    Restructure CI
    This will allow a user to run the individual (independent) phases locally if needed.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- ci/test.sh
+++ ci/test.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-K6T_VER=$1
-
 bold() { echo -e "\e[1m$@\e[0m" ; }
 red() { echo -e "\e[31m$@\e[0m" ; }
 green() { echo -e "\e[32m$@\e[0m" ; }
@@ -17,28 +15,10 @@ condTravisFold() {
 }
 
 timeout_while() { timeout $1 sh -c "while true; do $2 && break || : ; sleep 1 ; done" ; }
-
-k_wait_all_running() {
-  get_remaining_pods() { kubectl get pods --all-namespaces --field-selector=status.phase!=Running,status.phase!=Succeeded ; }
-  while [[ "$( get_remaining_pods | wc -l)" -gt 1 ]];
-  do
-    kubectl get pods --all-namespaces
-    sleep 6;
-  done ; }
+k_wait_all_running() { bash ci/wait-pods-ok; }
 
 {
   set -xe
-
-  kubectl create configmap -n kube-system kubevirt-config --from-literal debug.useEmulation=true || :
-
-  kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/$K6T_VER/kubevirt.yaml ;
-
-  kubectl api-versions | grep kubevirt.io
-
-  condTravisFold k_wait_all_running
-
-  kubectl describe nodes
-  kubectl describe pods --all-namespaces
 
   kubectl apply -f manifests/vm.yaml
 
@@ -57,7 +37,5 @@ k_wait_all_running() {
 
   set +xe
 }
-
-#curl -Lo virtctl https://github.com/kubevirt/kubevirt/releases/download/v$K6T_VER/virtctl-v$K6T_VER-linux-amd64 && chmod +x virtctl && sudo mv virtctl /usr/local/bin
 
 PASS
